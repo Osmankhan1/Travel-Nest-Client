e87b2327bd830b5bbd2ef28f8b357edec3cb049b
osmankhanakash <khanosman494@gmail.com>
2023-11-08 15:18:07 +0600

    add js file, jsx file, map jsx file, login jsx file, subscribe jsx file, packege jsx file, Navbar jsx file,Register jsx file, Banner jsx file, Rooms jsx file, myBookings jsx file, Testomonial jsx file,main jsx file, some css file, some image, some json file, rout file, Provider file , some folder adding , and coding file adding

--- src/Pages/MyBookings/MyBookings.jsx
+++ src/Pages/MyBookings/MyBookings.jsx
@@ -2,24 +2,28 @@ import cover from '../../assets/img/rooms/bedroom (2).jpg'
 
 const MyBookings = () => {
     return (
+
+
+
         <div className="mt-10 max-w-7xl mx-auto">
             <h1 className='text-[42px] font-bold text-center '>My Booking <span className='text-[#FF3811]'>List</span> </h1>
 
-            <div className="card w-96 bg-base-100 shadow-xl mt-16 mb-36">
-                <figure><img src={cover}/></figure>
+
+            <div className="card w-1/2 lg:card-side bg-base-100 shadow-xl mt-16 mb-36">
+                <figure><img src={cover} alt="Album" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
+                    <h2 className="card-title">New album is released!</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <div className=" flex gap-3 justify-end">
                         <button className="btn btn-sm border-2 border-emerald-600 hover:bg-[#FF3811] hover:text-white">View</button>
                         <button className="btn btn-sm border-2 border-emerald-600 hover:bg-[#FF3811] hover:text-white">Edit</button>
                         <button className="btn btn-sm border-2 border-emerald-600 hover:bg-[#FF3811] hover:text-white">Update</button>
                     </div>
                 </div>
             </div>
+
+
+          
 
         </div>
     );
